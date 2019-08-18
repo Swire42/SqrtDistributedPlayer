ae7237e7b2cc75c19263e85747854549a8747792
Victor <v_m_1732@orange.fr>
2019-08-18 18:23:36 +0200

    Fixed termux_setup.sh

--- termux_setup.sh
+++ termux_setup.sh
@@ -1,4 +1,5 @@
 pkg install pulseaudio sox termux-api python
 pip install mutagen
 echo alias sdp=\"python $PWD/sdp.py\" >> ~/.bashrc
+source ~/.bashrc
 echo Please make sure to have \"Termux - API\" installed (You can get it in Google Play Store)
